c84674a152fbdb69d970219f8997f32b4f04e684
Mikhail Konovalov <mikhail@canva.com>
2020-11-28 01:24:02 +1100

    include lib

--- fido2_other.go
+++ fido2_other.go
@@ -4,6 +4,6 @@ package libfido2
 #cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu -lfido2
 #cgo linux CFLAGS: -I/usr/include/fido
 #cgo windows LDFLAGS: -L${SRCDIR}/windows/lib -lfido2
-#cgo windows CFLAGS: -I${SRCDIR}/windows/include
+#cgo windows CFLAGS: -I${SRCDIR}/include
 */
 import "C"
