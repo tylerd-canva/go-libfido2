07a7329ef2d091fb16d82a684a058ce050b6460a
Mikhail Konovalov <mikhail@canva.com>
2020-12-04 11:48:09 +1100

    linux in nix

--- fido2_other.go
+++ fido2_other.go
@@ -1,8 +1,7 @@
 package libfido2
 
 /*
-#cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu -lfido2
-#cgo linux CFLAGS: -I/usr/include/fido
+#cgo linux LDFLAGS: -lfido2 -lcbor -lcrypto -ludev
 #cgo windows LDFLAGS: -L${SRCDIR}/windows/lib -lfido2
 #cgo windows CFLAGS: -I${SRCDIR}/include
 */
